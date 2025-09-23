66f3ea0fe66d4643d60b9a531def203a096bff10
boshuua <ws369808@weston.ac.uk>
2025-09-23 19:57:18 +0100

    Remove empty style block and ensure stylesheet link is present in the login page

--- index.php
+++ index.php
@@ -10,9 +10,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <style>
-        
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="login-container">
