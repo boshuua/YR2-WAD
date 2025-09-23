22217f5f13504629b5b35a3d5b1e1b52da5975b5
boshuua <ws369808@weston.ac.uk>
2025-09-23 18:36:57 +0100

    Fix connection error handling in database connection script

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -5,6 +5,10 @@ $username = "ws369808_JOSHK";
 $password = "t_k41N42q";
 $dbname = "ws369808_WAD";
 $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
+
+?>
