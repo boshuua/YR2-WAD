c3a282e3aae098e5e6a97f3384368916f41fa1cc
boshuua <ws369808@weston.ac.uk>
2026-02-13 15:06:22 +0000

    feat: add user 'My Courses' page with course listing and enrollment, and create admin user detail template.

--- cpd-portal/src/app/pages/user/my-courses/my-courses.component.ts
+++ cpd-portal/src/app/pages/user/my-courses/my-courses.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { ToastService } from '../../../core/services/toast.service';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-my-courses',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './my-courses.component.html',
   styleUrls: ['./my-courses.component.css']
 })
